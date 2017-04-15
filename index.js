557082ebec46b25c4d14286b60c1d90c925c9db3
Jerry Boyce <jokerdelta@yahoo.com>
2017-04-15 14:54:41 +0000

    Done.

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function doToElementsInArray(array,callback){
   array.forEach(callback)
 }
 
-function changeCompletely(element,index,array{
-  array[index] = (Math.random() * 100).toString() + '!!!')
+function changeCompletely(element,index,array){
+  array[index] = ((Math.random() * 100).toString() + '!!!')
 }
 
 var animals = ['dog', 'fish', 'cat']
